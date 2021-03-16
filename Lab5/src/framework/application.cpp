5943a9613a803e5e11b1da3b0bca765343f28039
rogerpurgimon <roger.purgimon01@estudiant.upf.edu>
2021-03-16 16:05:23 +0100

    Fins el 3

--- Lab5/src/framework/application.cpp
+++ Lab5/src/framework/application.cpp
@@ -19,6 +19,7 @@ Texture* texture = NULL;
 Texture* normalmap_texture = NULL;
 Light* light1 = NULL;
 Light* light2 = NULL;
+Light* light3 = NULL;
 Material* material = NULL;
 
 Vector3 ambient_light(60/255, 60/255, 60/255); //here we can store the global ambient light of the scene
@@ -29,6 +30,7 @@ float angley = 0;
 Vector2 first_pos;
 bool left_click = false;
 Vector2 d;
+int num_light = 1;
 
 enum Exercises { simple, task1, task2, task3, task4, task6 };
 Exercises exercise = simple;
@@ -86,9 +88,14 @@ void Application::init(void)
 	//load whatever you need here
 	light1 = new Light();
 	light1->position.set(20, 20, 10);
+	light1->diffuse_color.set(1.0f, 1.0f, 1.0f);
+	light1->specular_color.set(1.0f, 1.0f, 1.0f);
 	light2 = new Light();
 	material = new Material();
-	light2->position.set(-50, 10, 0);
+	light2->position.set(-5, 0, -5);
+	light2->diffuse_color.set(0.2f, 0.25f, 0.5f);
+	light2->specular_color.set(0.2f, 0.25f, 0.5f);
+	light3 = new Light();
 	lights.push_back(*light1);
 	lights.push_back(*light2);
 }
@@ -130,15 +137,16 @@ void Application::render(void)
 		//enable the shader
 		phong1_shader->enable();
 
-		model_matrix.setIdentity();
-		model_matrix.translate(0, 0, 0);
-		model_matrix.rotate(anglex, Vector3(0, 1, 0));
-		model_matrix.rotate(angley, Vector3(1, 0, 0));
-		phong1_shader->setMatrix44("model", model_matrix); //upload info to the shader
-		phong1_shader->setMatrix44("viewprojection", viewprojection); //upload info to the shader
-
-		phong1_shader->setTexture("u_texture", texture, 0); //set texture in slot 0
 		for (int i = 0; i < lights.size(); i++) {
+			model_matrix.setIdentity();
+			model_matrix.translate(0, 0, 0);
+			model_matrix.rotate(anglex, Vector3(0, 1, 0));
+			model_matrix.rotate(angley, Vector3(1, 0, 0));
+			phong1_shader->setMatrix44("model", model_matrix); //upload info to the shader
+			phong1_shader->setMatrix44("viewprojection", viewprojection); //upload info to the shader
+
+			phong1_shader->setTexture("u_texture", texture, 0); //set texture in slot 0
+		
 			phong1_shader->setUniform3("u_camera_pos", camera->eye);
 			phong1_shader->setUniform3("u_light_pos", lights[i].position);
 
@@ -151,7 +159,9 @@ void Application::render(void)
 			phong1_shader->setUniform3("u_Id", lights[i].diffuse_color);
 			phong1_shader->setUniform3("u_Is", lights[i].specular_color);
 
-			mesh->render(GL_TRIANGLES);
+			glDepthFunc(GL_LEQUAL);
+			if (i == 0) { glDisable(GL_BLEND); mesh->render(GL_TRIANGLES); }
+			else { glEnable(GL_BLEND); glBlendFunc(GL_ONE, GL_ONE); mesh->render(GL_TRIANGLES); glDisable(GL_BLEND); }
 		}
 		//disable shader
 		phong1_shader->disable();
@@ -162,16 +172,16 @@ void Application::render(void)
 	else if (exercise == task2) {
 		//enable the shader
 		phong2_shader->enable();
-
-		model_matrix.setIdentity();
-		model_matrix.translate(0, 0, 0);
-		model_matrix.rotate(anglex, Vector3(0, 1, 0));
-		model_matrix.rotate(angley, Vector3(1, 0, 0));
-		phong2_shader->setMatrix44("model", model_matrix); //upload info to the shader
-		phong2_shader->setMatrix44("viewprojection", viewprojection); //upload info to the shader
-
-		phong2_shader->setTexture("u_texture", texture, 0); //set texture in slot 0
 		for (int i = 0; i < lights.size(); i++) {
+			model_matrix.setIdentity();
+			model_matrix.translate(0, 0, 0);
+			model_matrix.rotate(anglex, Vector3(0, 1, 0));
+			model_matrix.rotate(angley, Vector3(1, 0, 0));
+			phong2_shader->setMatrix44("model", model_matrix); //upload info to the shader
+			phong2_shader->setMatrix44("viewprojection", viewprojection); //upload info to the shader
+
+			phong2_shader->setTexture("u_texture", texture, 0); //set texture in slot 0
+		
 			phong2_shader->setUniform3("u_camera_pos", camera->eye);
 			phong2_shader->setUniform3("u_light_pos", lights[i].position);
 
@@ -184,7 +194,9 @@ void Application::render(void)
 			phong2_shader->setUniform3("u_Id", lights[i].diffuse_color);
 			phong2_shader->setUniform3("u_Is", lights[i].specular_color);
 
-			mesh->render(GL_TRIANGLES);
+			glDepthFunc(GL_LEQUAL);
+			if (i == 0) { glDisable(GL_BLEND); mesh->render(GL_TRIANGLES); }
+			else { glEnable(GL_BLEND); glBlendFunc(GL_ONE, GL_ONE); mesh->render(GL_TRIANGLES); glDisable(GL_BLEND); }
 		}
 		//disable shader
 		phong2_shader->disable();
@@ -195,17 +207,17 @@ void Application::render(void)
 	else if (exercise == task3) {
 		//enable the shader
 		phong3_shader->enable();
-
-		model_matrix.setIdentity();
-		model_matrix.translate(0, 0, 0);
-		model_matrix.rotate(anglex, Vector3(0, 1, 0));
-		model_matrix.rotate(angley, Vector3(1, 0, 0));
-		phong3_shader->setMatrix44("model", model_matrix); //upload info to the shader
-		phong3_shader->setMatrix44("viewprojection", viewprojection); //upload info to the shader
-
-		phong3_shader->setTexture("u_texture", texture, 0); //set texture in slot 0
-		phong3_shader->setTexture("u_normalmap", normalmap_texture, 1); //set texture in slot 1
 		for (int i = 0; i < lights.size(); i++) {
+			model_matrix.setIdentity();
+			model_matrix.translate(0, 0, 0);
+			model_matrix.rotate(anglex, Vector3(0, 1, 0));
+			model_matrix.rotate(angley, Vector3(1, 0, 0));
+			phong3_shader->setMatrix44("model", model_matrix); //upload info to the shader
+			phong3_shader->setMatrix44("viewprojection", viewprojection); //upload info to the shader
+
+			phong3_shader->setTexture("u_texture", texture, 0); //set texture in slot 0
+			phong3_shader->setTexture("u_normalmap", normalmap_texture, 1); //set texture in slot 1
+		
 			phong3_shader->setUniform3("u_camera_pos", camera->eye);
 			phong3_shader->setUniform3("u_light_pos", lights[i].position);
 
@@ -218,7 +230,9 @@ void Application::render(void)
 			phong3_shader->setUniform3("u_Id", lights[i].diffuse_color);
 			phong3_shader->setUniform3("u_Is", lights[i].specular_color);
 
-			mesh->render(GL_TRIANGLES);
+			glDepthFunc(GL_LEQUAL);
+			if (i == 0) { glDisable(GL_BLEND); mesh->render(GL_TRIANGLES); }
+			else { glEnable(GL_BLEND); glBlendFunc(GL_ONE, GL_ONE); mesh->render(GL_TRIANGLES); glDisable(GL_BLEND); }
 		}
 		//disable shader
 		phong3_shader->disable();
@@ -243,6 +257,68 @@ void Application::update(double seconds_elapsed)
 		}
 	}
 
+	if (keystate[SDL_SCANCODE_RIGHT]) {
+		if (exercise == 1 || exercise == 2) light1->position = light1->position + Vector3(1, 0, 0) * seconds_elapsed * 10.0;
+		else {
+			switch (num_light) {
+			case 1:
+				lights[0].position = lights[0].position + Vector3(1, 0, 0) * seconds_elapsed * 10.0;
+				lights[1].position = lights[1].position - Vector3(1, 0, 0) * seconds_elapsed * 10.0;
+			case 2:
+				lights[1].position = lights[1].position + Vector3(1, 0, 0) * seconds_elapsed * 10.0;
+				lights[2].position = lights[2].position - Vector3(1, 0, 0) * seconds_elapsed * 10.0;
+			case 3:
+				lights[2].position = lights[2].position + Vector3(1, 0, 0) * seconds_elapsed * 10.0;
+			}
+		}
+	}
+	else if (keystate[SDL_SCANCODE_LEFT]) {
+		if (exercise == 1 || exercise == 2) light1->position = light1->position + Vector3(-1, 0, 0) * seconds_elapsed * 10.0;
+		else {
+			switch (num_light) {
+			case 1:
+				lights[0].position = lights[0].position + Vector3(-1, 0, 0) * seconds_elapsed * 10.0;
+				lights[1].position = lights[1].position - Vector3(-1, 0, 0) * seconds_elapsed * 10.0;
+			case 2:
+				lights[1].position = lights[1].position + Vector3(-1, 0, 0) * seconds_elapsed * 10.0;
+				lights[2].position = lights[2].position - Vector3(-1, 0, 0) * seconds_elapsed * 10.0;
+
+			case 3:
+				lights[2].position = lights[2].position + Vector3(-1, 0, 0) * seconds_elapsed * 10.0;
+			}
+		}
+	}
+	if (keystate[SDL_SCANCODE_UP]) {
+		if (exercise == 1 || exercise == 2) light1->position = light1->position + Vector3(0, 1, 0) * seconds_elapsed * 10.0;
+		else {
+			switch (num_light) {
+			case 1:
+				lights[0].position = lights[0].position + Vector3(0, 1, 0) * seconds_elapsed * 10.0;
+				lights[1].position = lights[1].position - Vector3(0, 1, 0) * seconds_elapsed * 10.0;
+			case 2:
+				lights[1].position = lights[1].position + Vector3(0, 1, 0) * seconds_elapsed * 10.0;
+				lights[2].position = lights[2].position - Vector3(0, 1, 0) * seconds_elapsed * 10.0;
+			case 3:
+				lights[2].position = lights[2].position + Vector3(0, 1, 0) * seconds_elapsed * 10.0;
+			}
+		}
+	}
+	else if (keystate[SDL_SCANCODE_DOWN]) {
+		if (exercise == 1 || exercise == 2) light1->position = light1->position + Vector3(0, -1, 0) * seconds_elapsed * 10.0;
+		else {
+			switch (num_light) {
+			case 1:
+				lights[0].position = lights[0].position + Vector3(0, -1, 0) * seconds_elapsed * 10.0;
+				lights[1].position = lights[1].position - Vector3(0, -1, 0) * seconds_elapsed * 10.0;
+			case 2:
+				lights[1].position = lights[1].position + Vector3(0, -1, 0) * seconds_elapsed * 10.0;
+				lights[2].position = lights[2].position - Vector3(0, -1, 0) * seconds_elapsed * 10.0;
+			case 3:
+				lights[2].position = lights[2].position + Vector3(0, -1, 0) * seconds_elapsed * 10.0;
+			}
+		}
+	}
+
 	if (keystate[SDL_SCANCODE_F]) { camera->fov += 20 * seconds_elapsed; }
 	if (keystate[SDL_SCANCODE_G]) { camera->fov -= 20 * seconds_elapsed; }
 
@@ -262,6 +338,14 @@ void Application::onKeyPressed( SDL_KeyboardEvent event )
 		case SDL_SCANCODE_2: exercise = task2; std::cout << "\n\nTASK 2" << std::endl; break;
 		case SDL_SCANCODE_3: exercise = task3; std::cout << "\n\nTASK 3" << std::endl; break;
         case SDL_SCANCODE_ESCAPE: exit(0); break; //ESC key, kill the app
+		case SDL_SCANCODE_X: {
+			if (num_light == 1) { num_light = 2; }
+			else if (num_light == 2) { num_light = 3; }
+			else { num_light = 1; }
+//			printf("\nLight1 Position =(%.2f, %.2f, %.2f)\nLight2 Position =(%.2f, %.2f, %.2f)\nLight3 Position =(%.2f, %.2f, %.2f)\n", lights[0].position.x, lights[0].position.y, lights[0].position.z,
+//			lights[1].position.x, lights[1].position.y, lights[1].position.z, lights[2].position.x, lights[2].position.y, lights[2].position.z);
+//			printf("num_light =%d\n", num_light);
+		}
 	}
 
 }
